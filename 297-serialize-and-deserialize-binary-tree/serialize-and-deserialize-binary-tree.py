966b7007baa0332d9b8486bc5c8f4f0a1f931bbc
Sohan Suchdev <suchdevsohan@gmail.com>
2026-07-21 01:54:37 +0100

    Time: 92 ms (42.22%) | Memory: 22.6 MB (59.73%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+    def serialize(self, root):
+        vals = []
+        
+        def dfs(node):
+            if not node:
+                vals.append("_")
+                return
+                
+            vals.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+            
+        dfs(root)
+        return ",".join(vals)
+        
+
+    def deserialize(self, data):
+        vals = iter(data.split(","))
+        
+        def build():
+            val = next(vals)
+            
+            if val == "_":
+                return None
+                
+            node = TreeNode(int(val))
+            
+            node.left = build()
+            node.right = build()
+            
+            return node
+            
+        return build()
