93e5c19f71f61a73af2014be70ed9608545b3adf
Sohan Suchdev <suchdevsohan@gmail.com>
2026-08-13 22:05:55 +0100

    Time: 29 ms (58.13%) | Memory: 20.5 MB (31.82%) - LeetSync

--- 2475-largest-palindromic-number/largest-palindromic-number.py
+++ 2475-largest-palindromic-number/largest-palindromic-number.py
@@ -1,23 +1,36 @@
 from collections import Counter
+
 class Solution:
     def largestPalindromic(self, num: str) -> str:
+        # Count and sort descending
         cnt = Counter(num)
-        s_cnt = sorted((cnt.items()), reverse = True)
+        s_cnt = sorted(cnt.items(), reverse=True)
 
         odd = ''
         odd_flag = False
-
         ans = ''
+
+        # Build the left side of the palindrome
         for x, y in s_cnt:
             if y % 2 == 1 and not odd_flag:
                 odd_flag = True
                 odd = x
-            ans += x*(y//2)
+            ans += x * (y // 2)
 
-        ans = ans.lstrip('0')
+        # Manually remove leading zeros
+        # If ans starts with '0', it means ONLY '0's were added to the left half.
+        clean_ans = ""
+        for char in ans:
+            if clean_ans == "" and char == '0':
+                continue # Skip leading zeros
+            clean_ans += char
+        ans = clean_ans
 
+        # Construct final answer: Left half + Middle + Reversed Left half
         ans = ans + odd + ans[::-1]
 
+        # If everything was stripped away, the only valid answer is '0'
         if ans == '':
             return '0'
+            
         return ans
